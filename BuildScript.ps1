4f24d71880bcdbcf72d671757290b6db09b608c0
Tobias Wiesendanger <tobias.wiesendanger@mum.ch>
2023-07-17 09:39:15 +0200

    feat: added buildscript / does need ps2exe.ps1

---
+++ BuildScript.ps1
@@ -0,0 +1,6 @@
+﻿$pathExeCreater = "D:\OneDrive - MuM - OM\TWIProgrammierung\Autodesk\CreateExe\ps2exe.ps1"
+$version = "0.96"
+$pathMainScript = "C:\MuM_Git\ADSKDashboardPS\ADSKDashboard.ps1"
+$pathOutput = "C:\MuM_Git\ADSKDashboardPS\builds\ADSKDashboard_$($version).exe"
+$pathIcon = "C:\MuM_Git\ADSKDashboardPS\res\ADSKDashboard_Icon.ico"
+. $pathExeCreater -inputFile $pathMainScript -outputFile $pathOutput -iconFile $pathIcon -noConsole -noOutput -title "ADSK Dashboard" -copyright "Tobias Wiesendanger" -verbose -version $version
