dc2c0deb3a5e37581cc6e3fa9284c062d158fc55
Tobias Wiesendanger <tobias.wiesendanger@mum.ch>
2024-04-15 14:57:04 +0200

    fix: paths are now relativ

--- BuildScript.ps1
+++ BuildScript.ps1
@@ -1,7 +1,7 @@
-﻿$pathExeCreater = "D:\OneDrive - MuM - OM\01_Develope\01_Powershell\Buildscript\PS2EXE-master\Module\ps2exe.ps1"
+﻿$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $version = "0.97"
-$pathMainScript = "C:\Work\ADSKDashboardPS\ADSKDashboard.ps1"
-$pathOutput = "C:\Work\ADSKDashboardPS\builds\ADSKDashboard_$($version).exe"
-$pathIcon = "C:\Work\ADSKDashboardPS\res\ADSKDashboard_Icon.ico"
-. $pathExeCreater 
+$pathMainScript = Join-Path $scriptPath "ADSKDashboard.ps1"
+$pathOutput = Join-Path $scriptPath "builds\ADSKDashboard_$version.exe"
+$pathIcon = Join-Path $scriptPath "res\ADSKDashboard_Icon.ico"
+
 Invoke-ps2exe -inputFile $pathMainScript -outputFile $pathOutput -iconFile $pathIcon -noConsole -noOutput -title "ADSK Dashboard" -copyright "Tobias Wiesendanger" -verbose -version $version
